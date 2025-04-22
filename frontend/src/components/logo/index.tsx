f4c41b5722f7e3e3c674b0adc0d03af8ca8ebef8
PrajapatiRoshan <roshanprajapati6097@gmail.com>
2025-04-22 20:34:50 +0530

    icon updaed

--- frontend/src/components/logo/index.tsx
+++ frontend/src/components/logo/index.tsx
@@ -1,17 +1,23 @@
-import { AudioWaveform } from "lucide-react";
-import { Link } from "react-router-dom";
+import { AudioWaveform } from 'lucide-react';
+import { Link } from 'react-router-dom';
 
 const Logo = (props: { url?: string }) => {
-  const { url = "/" } = props;
+  const { url = '/' } = props;
   return (
     <div className="flex items-center justify-center sm:justify-start">
       <Link to={url}>
-        <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+        {/* <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
           <AudioWaveform className="size-4" />
-        </div>
+        </div> */}
+        <img
+          src="images/project-management.png"
+          alt="f"
+          className="inline-block w-[40px]"
+        />
       </Link>
     </div>
   );
 };
 
 export default Logo;
+
